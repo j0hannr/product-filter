3e782dc915baf15b5db972414d1c4e97c8759170
j0hannr <johann.rohn@gmail.com>
2022-07-13 16:59:52 +0200

    🏗  Git Action

--- playwright.config.js
+++ playwright.config.js
@@ -39,7 +39,7 @@ const config = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ||  "http://localhost:3002",
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ||  "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
